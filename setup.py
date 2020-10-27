bba74051631a6729fd40b24d1353cd688fad0074
narek <narek.hnh@gmail.com>
2020-10-27 05:16:53 +0400

    Minor fix of lxml

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='Data team',
     author_email='data@teamable.com',
     packages=find_packages(exclude=['tests']),
-    install_requires=["requests==2.21.0", "bs4==0.0.1", "lxml==4.3.4","tqdm",
+    install_requires=["requests==2.21.0", "bs4==0.0.1", "lxml","tqdm",
                       "pandas==0.24.2", "brotli==1.0.7", "selenium==3.141.0"],  # Optional
 )
